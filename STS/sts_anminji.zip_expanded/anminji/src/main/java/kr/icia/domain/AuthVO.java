0e7031fd121f1a8c3abe54cc696594e41600a1a4
Anhyeon-jong <93761762+Anhyeon-jong@users.noreply.github.com>
2021-11-17 20:45:00 +0900

    Delete STS/sts_anminji.zip_expanded/anminji directory

--- STS/sts_anminji.zip_expanded/anminji/src/main/java/kr/icia/domain/AuthVO.java
+++
@@ -1,9 +0,0 @@
-package kr.icia.domain;
-
-import lombok.Data;
-
-@Data
-public class AuthVO {
-	private String userid;
-	private String auth;
-}
--- STS/sts_anminji.zip_expanded/anminji/src/main/java/kr/icia/mapper/MemberMapper.java
+++
@@ -1,7 +0,0 @@
-package kr.icia.mapper;
-
-import kr.icia.domain.MemberVO;
-
-public interface MemberMapper {
-	public MemberVO read(String userid);
-}
